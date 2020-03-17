ceec1b8c0f24dbf96f1bc8e2fe56a0f287b290e5
Adam Aposhian <adam.l.aposhian@gmail.com>
2020-03-17 15:37:47 -0600

    Initial implementation of logger

---
+++ index.js
@@ -0,0 +1,58 @@
+const winston = require('winston')
+
+const levels = [
+  'debug',
+  'info',
+  'warn',
+  'error'
+]
+
+const DEFAULT_LOG_LEVEL = 'error'
+
+const logPrefix = () => {
+  return process.env.LOG_PREFIX ? `${process.env.LOG_PREFIX}: ` : ''
+}
+
+const logger = winston.createLogger({
+  transports: [
+    new winston.transports.Console()
+  ],
+  exitOnError: false,
+  format: format.combine(
+    format.align(),
+    format.printf(info => `${logPrefix()}${info.level}: ${info.message}`)
+  )
+})
+
+const setLogLevel = (logLevel) => {
+  if (levels.includes(logLevel)) {
+    process.env.LOG_LEVEL = logLevel
+  }
+}
+
+const setLogPrefix = (logPrefix) => {
+  process.env.LOG_PREFIX = logPrefix
+}
+
+const update = () => {
+  logger.level = process.env.LOG_LEVEL || DEFAULT_LOG_LEVEL
+  logger
+}
+
+const wrapFunction = (logFunction, ...args) => {
+  update()
+  logFunction(process.env.LOG_PREFIX, ...args)
+}
+
+const log = {
+  debug: (...args) => wrapFunction(logger.debug, ...args),
+  info: (...args) => wrapFunction(logger.info, ...args),
+  warn: (...args) => wrapFunction(logger.warn, ...args),
+  error: (...args) => wrapFunction(logger.error, ...args)
+}
+
+module.exports = {
+  log,
+  setLogLevel,
+  setLogPrefix
+}
